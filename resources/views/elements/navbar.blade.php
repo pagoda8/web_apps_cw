f2848f81463392224fdeac66a9cae1ab53041177
Wojtek <wojteklag@gmail.com>
2023-05-03 19:02:23 +0100

    improved navbar

--- resources/views/elements/navbar.blade.php
+++ resources/views/elements/navbar.blade.php
@@ -2,11 +2,22 @@
 	<div class="container-fluid">
 		<div class="navbar-nav">
 		  <a class="nav-link" href="/">Car Licitations</a>
-		  <a class="nav-link" href="/my_profile">My Profile</a>
-		  <a class="nav-link" href="/create_licitation">Create Licitation</a>
-		  <a class="nav-link" href="/login">Log in</a>
-		  <a class="nav-link" href="/register">Register</a>
-		  <a class="nav-link" href="/logout">Log out</a>
+		  @if(auth()->check())
+		  	&emsp;
+		  	<a class="nav-link" href="/my_profile">My Profile</a>
+			&emsp;
+		  	<a class="nav-link" href="/create_licitation">Create Licitation</a>
+		  @endif
+		  @if(!auth()->check())
+		  	&emsp;
+		  	<a class="nav-link" href="/login">Log in</a>
+		  	&emsp;
+			<a class="nav-link" href="/register">Register</a>
+		  @endif
+		  @if(auth()->check())
+		  	&emsp;
+		  	<a class="nav-link" href="/logout">Log out</a>
+		  @endif
 		</div>
 	</div>
 </nav>
